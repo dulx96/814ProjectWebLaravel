fdc5d5669a5d033e0e2d1fa52dd79d6d7abd5a96
xuanduvip96 <dulx96@gmail.com>
2017-12-18 17:05:53 +0700

    fix css

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URI', 'https://nam-chym.herokuapp.com'),
+    'url' => env('APP_URL', 'https://nam-chym.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
